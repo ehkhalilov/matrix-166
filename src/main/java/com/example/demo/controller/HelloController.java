76d7bc20403c6618af3255302e702b9aba17cf98
ehkhalilov <elnur.khalilov@pashabank.az>
2025-07-26 20:45:17 +0400

    change controller
    # Conflicts:
    #	src/main/java/com/example/demo/controller/HelloController.java

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -8,6 +8,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello from CI/CD";
+        System.out.println("Hello");
+        return "Hello from CI/CD!!!";
     }
 }
